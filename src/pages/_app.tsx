871a8480b036f40eb56664e5103d9828ab8ecf11
Bushraabir <muzahidulislamabir123@gmail.com>
2025-08-05 17:08:35 +0600

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Head>
+
+      <title>PhyCode</title>
+      <meta name="description" content="Merge of Physics and Coding concepts" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="UTF-8" />
+
+    </Head>
+    <Component {...pageProps} />
+    </>
+  );
 }
