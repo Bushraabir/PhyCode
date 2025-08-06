ddfbb9aeecc6e30efc93951c3c834f02b58f070c
Bushraabir <muzahidulislamabir123@gmail.com>
2025-08-06 12:13:50 +0600

    updated

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -15,23 +15,29 @@ const AuthModal: React.FC = () => {
   if (!authModal.isOpen) return null;
 
   return (
-    <>
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center">
+      {/* Glassmorphism background */}
       <div
-        className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60'
+        className="absolute inset-0 bg-black/30 backdrop-blur-sm"
         onClick={closeModal}
       ></div>
-      <div className='w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center'>
-        <div className='relative w-full h-full mx-auto flex items-center justify-center'>
-          <div className='bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6'>
-            <div className='flex justify-end p-2'>
-              <button
-                type='button'
-                className='bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white'
-                onClick={closeModal}
-              >
-                <IoClose className='h-5 w-5' />
-              </button>
-            </div>
+
+      {/* Modal */}
+      <div className="z-50 w-full sm:w-[450px] mx-4">
+        <div className="bg-gradient-to-b from-brand-orange to-slate-900 rounded-lg shadow-xl relative text-white">
+          {/* Close Button */}
+          <div className="flex justify-end p-2">
+            <button
+              type="button"
+              className="bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white"
+              onClick={closeModal}
+            >
+              <IoClose className="h-5 w-5" />
+            </button>
+          </div>
+
+          {/* Form */}
+          <div className="px-6 pb-6">
             {authModal.type === "login" ? (
               <Login />
             ) : authModal.type === "register" ? (
@@ -42,7 +48,7 @@ const AuthModal: React.FC = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
